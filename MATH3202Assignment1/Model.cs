dd332b2332d97455a259a8382bb118c7930e69d4
martinrhan <martinrhan@126.com>
2023-03-19 09:58:03 +0800

    fixed bug, now can pass com3 and com4

--- MATH3202Assignment1/Model.cs
+++ MATH3202Assignment1/Model.cs
@@ -46,8 +46,8 @@ namespace MATH3202Assignment1 {
         internal static Model FromFile1Repeat() {
             IEnumerable<string[]> lines = ReadLines("nodes.csv").Select(str => str.Split(',')).ToArray();
             var nodes = lines.Select(strs => new Node() { X = double.Parse(strs[1]), Y = double.Parse(strs[2]) }).ToArray();
-            var demands = lines.SelectMany(strs => Enumerable.Repeat(double.Parse(strs[3]), 5)).ToArray();
-            return new Model(nodes, demands, 5);
+            var demands = lines.SelectMany(strs => Enumerable.Repeat(double.Parse(strs[3]), 14)).ToArray();
+            return new Model(nodes, demands, 14);
         }
 
         internal static Model FromFile2() {
@@ -57,21 +57,29 @@ namespace MATH3202Assignment1 {
             return new(nodes, demands.ToArray(), 14);
         }
 
+        internal static Model FromFile2SingleDay(int day) {
+            IEnumerable<string[]> lines = ReadLines("nodes2.csv").Select(str => str.Split(',')).ToArray();
+            var nodes = lines.Select(strs => new Node() { X = double.Parse(strs[1]), Y = double.Parse(strs[2]) }).ToArray();
+            var demands = lines.Select(strs => strs[3 + day]).Select(double.Parse).ToArray();
+            return new(nodes, demands.ToArray(), 1);
+        }
+
         Node[] Nodes { get; }
         double[] demands;
         double GetDemand(int day, int node) => demands[node * DayAmount + day];
         int DayAmount { get; }
 
-        internal double FindOptimal(bool pipelineLimit = true, bool log = true) {
+        internal double FindOptimal(LogMode logMode = LogMode.Detailed, double pipelineLimit = double.PositiveInfinity, double supplierLimit = double.PositiveInfinity) {
             GRBModel model = new GRBModel(Env);
             GRBVar[,] variables = new GRBVar[DayAmount, Pipelines.Length];
+            GRBVar[,] variables_suppliers = new GRBVar[DayAmount, Suppliers.Length];
             GRBLinExpr[] dailyCosts = new GRBLinExpr[DayAmount];
-            for(int i_day = 0;  i_day < DayAmount; i_day++) {
+            for (int i_day = 0; i_day < DayAmount; i_day++) {
                 dailyCosts[i_day] = new GRBLinExpr();
                 for (int i_pipeline = 0; i_pipeline < Pipelines.Length; i_pipeline++) {
                     ref Pipeline pipeline = ref Pipelines[i_pipeline];
-                    variables[i_day,i_pipeline] = model.AddVar(0, pipelineLimit ? 313 : double.PositiveInfinity, default, GRB.CONTINUOUS, $"D{i_day}P{pipeline}:{pipeline.Node0}->{pipeline.Node1}");
-                    dailyCosts[i_day].AddTerm(0.01 * Pipelines[i_pipeline].Length, variables[i_day,i_pipeline]);
+                    variables[i_day, i_pipeline] = model.AddVar(0, pipelineLimit, default, GRB.CONTINUOUS, $"D{i_day}P{pipeline}:{pipeline.Node0}->{pipeline.Node1}");
+                    dailyCosts[i_day].AddTerm(0.01 * Pipelines[i_pipeline].Length, variables[i_day, i_pipeline]);
                 }
                 for (int i_node = 0; i_node < Nodes.Length; i_node++) {
                     ref Node node = ref Nodes[i_node];
@@ -82,32 +90,41 @@ namespace MATH3202Assignment1 {
                     foreach (int i_pipeline in node.OutPipelines) {
                         netInflow.AddTerm(-1, variables[i_day, i_pipeline]);
                     }
-                    Supplier supplier;
-                    try {
-                        supplier = Suppliers.First(s => s.Node == i_node);
-                        model.AddConstr(GetDemand(i_day, i_node) - netInflow <= supplier.Capacity, "CapacityConstraint");
-                        dailyCosts[i_day].Add((GetDemand(i_day, i_node) - netInflow) * supplier.Cost);
-                    } catch (InvalidOperationException) {
-                        model.AddConstr(netInflow >= GetDemand(i_day, i_node), "DemandConstraint");
+                    int i_supplier = Enumerable.Range(0, Suppliers.Length).FirstOrDefault(i => Suppliers[i].Node == i_node, -1);
+                    if (i_supplier == -1) {
+                        model.AddConstr(netInflow >= GetDemand(i_day, i_node), $"DemandConstraintT{i_day}N{i_node}");
+                    } else {
+                        ref Supplier supplier = ref Suppliers[i_supplier];
+                        variables_suppliers[i_day, i_supplier] = model.AddVar(0, supplier.Capacity, default, GRB.CONTINUOUS, $"S{i_supplier}");
+                        model.AddConstr(netInflow + variables_suppliers[i_day, i_supplier] >= GetDemand(i_day, i_node), $"DemandConstraintT{i_day}N{i_node}");
+                        dailyCosts[i_day].Add(variables_suppliers[i_day, i_supplier] * supplier.Cost);
                     }
                 }
+            }
+            for (int i_supplier = 0; i_supplier < Suppliers.Length; i_supplier++) {
+                GRBLinExpr totalSupply = new ();
+                for (int i_day = 0; i_day < DayAmount; i_day++) {
+                    totalSupply.AddTerm(1, variables_suppliers[i_day, i_supplier]);
+                }
+                model.AddConstr(totalSupply <= supplierLimit, $"SupplierConstraint{i_supplier}");
             }
             GRBLinExpr totalCost = new GRBLinExpr();
             foreach (GRBLinExpr cost in dailyCosts) totalCost.Add(cost);
             model.SetObjective(totalCost, GRB.MINIMIZE);
             model.Optimize();
 
-            if (log) {
+            if (logMode != LogMode.None) {
                 for (int i_day = 0; i_day < DayAmount; i_day++) {
                     Console.WriteLine("_______________");
                     Console.WriteLine($"Day{i_day},Cost:{dailyCosts[i_day].Value}");
+                    if (logMode == LogMode.Brief) continue;
                     string format = "{0,-5}{1,-8}{2,-8}{3,-20}{4,-20}{5,-20}";
                     Console.WriteLine(format, "Node", "|Demand", "|Actual", "|Inflows", "|OutFlows", "|Errors");
                     for (int i_node = 0; i_node < Nodes.Length; i_node++) {
                         string[] output;
                         ref Node node = ref Nodes[i_node];
-                        IEnumerable<(int, double)> inflows = node.InPipelines.Select(i => (Pipelines[i].Node0, variables[i_day,i].X)).ToArray();
-                        IEnumerable<(int, double)> outflows = node.OutPipelines.Select(i => (Pipelines[i].Node1, variables[i_day,i].X)).ToArray();
+                        IEnumerable<(int, double)> inflows = node.InPipelines.Select(i => (Pipelines[i].Node0, variables[i_day, i].X)).ToArray();
+                        IEnumerable<(int, double)> outflows = node.OutPipelines.Select(i => (Pipelines[i].Node1, variables[i_day, i].X)).ToArray();
                         var inflows_simplified = inflows.Where(t => t.Item2.ToString("#.##") != "").ToArray();
                         var outflows_simplified = outflows.Where(t => t.Item2.ToString("#.##") != "").ToArray();
                         output = new string[]{
@@ -125,8 +142,11 @@ namespace MATH3202Assignment1 {
                     }
                 }
                 Console.WriteLine("OptimalCost:" + model.ObjVal);
+                Console.WriteLine("OptimalCost:" + dailyCosts.Select(c => c.Value).Sum());
             }
             return model.ObjVal;
         }
     }
+
+    public enum LogMode { None, Brief, Detailed }
 }
--- MATH3202Assignment1/Program.cs
+++ MATH3202Assignment1/Program.cs
@@ -3,11 +3,12 @@ using MATH3202Assignment1;
 
 
 Action[] Communications = {
-    () => Model.FromFile1().FindOptimal(false),
     () => Model.FromFile1().FindOptimal(),
-    () => Model.FromFile2().FindOptimal(),
-
+    () => Model.FromFile1().FindOptimal(pipelineLimit: 313),
+    () => Model.FromFile2().FindOptimal(pipelineLimit: 313),
+    () => Model.FromFile2().FindOptimal(pipelineLimit: 313, supplierLimit: 7688)
 };
 
-Communications[2]();
+Communications[3]();
+//Model.FromFile2SingleDay(13).FindOptimal();
 //Model.FromFile1Repeat().FindOptimal();
